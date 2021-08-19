b6f784d077df13907d485386c200eb6d629debe7
Pete Taylor <pete.jc.taylor@hey.com>
2021-08-18 23:39:32 -0400

    adding sidekiq for active job runner

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,7 @@ module TaskerRails
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.active_job.queue_adapter = :sidekiq
 
     # Configuration for the application, engines, and railties goes here.
     #
