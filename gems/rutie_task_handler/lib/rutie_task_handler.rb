4ad1752c88770b3c16c8f6782f5309721445b19c
Pete Taylor <pete.jc.taylor@hey.com>
2021-10-09 21:03:03 -0400

    updating dylib / so path

--- gems/rutie_task_handler/lib/rutie_task_handler.rb
+++ gems/rutie_task_handler/lib/rutie_task_handler.rb
@@ -7,5 +7,5 @@ require 'rutie'
 module RutieTaskHandler
   class Error < StandardError; end
 
-  Rutie.new(:rutie_task_handler).init 'Init_rutie_task_handler', __dir__
+  Rutie.new(:rutie_task_handler, { lib_path: File.join(__dir__, '../lib/rutie_task_handler') }).init 'Init_rutie_task_handler', __dir__
 end
