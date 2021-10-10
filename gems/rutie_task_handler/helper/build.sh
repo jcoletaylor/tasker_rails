5dd2fd8c63ff7bc9e8ff7c543676f76fcca696f0
Pete Taylor <pete.jc.taylor@hey.com>
2021-10-10 00:21:56 -0400

    minor update to build sh

--- gems/rutie_task_handler/helper/build.sh
+++ gems/rutie_task_handler/helper/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # these are just reminders, don't really run this script, your paths aren't likely to be mine
-docker build -t rutie_linux_so_builder .
-docker run -t -i -v ~/projects/tasker/tasker_rails/gems/rutie_task_handler:/source rutie_linux_so_builder /bin/bash
+docker build -t rutie-linux-builder .
+docker run -t -i -v ~/projects/tasker/tasker_rails/gems/rutie_task_handler:/source rutie-linux-builder /bin/bash
 
 # reminder: then run this
 cargo build --release --target x86_64-unknown-linux-gnu
